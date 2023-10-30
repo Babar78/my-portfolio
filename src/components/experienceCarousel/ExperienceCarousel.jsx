d47472037800dd3c64e85a08bbaf256bbf98f232
Muhammad Babar <mbabar.bese20seecs@seecs.edu.pk>
2023-10-30 05:12:20 +0500

    Experience Carousel Autoplay Functionality Added

--- src/components/experienceCarousel/ExperienceCarousel.jsx
+++ src/components/experienceCarousel/ExperienceCarousel.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import Carousel from "react-spring-3d-carousel";
-import { useState, useEffect } from "react";
-import { config } from "react-spring";
+import React, { useState, useEffect } from 'react';
+import Carousel from 'react-spring-3d-carousel';
+import { config } from 'react-spring';
 
 function ExperienceCarousel(props) {
   const table = props.cards.map((element, index) => {
@@ -10,13 +9,31 @@ function ExperienceCarousel(props) {
 
   const [offsetRadius, setOffsetRadius] = useState(4);
   const [showArrows, setShowArrows] = useState(false);
-  const [goToSlide, setGoToSlide] = useState(null);
+  const [goToSlide, setGoToSlide] = useState(0); // Start from the first slide
   const [cards] = useState(table);
 
   useEffect(() => {
     setOffsetRadius(props.offset);
     setShowArrows(props.showArrows);
   }, [props.offset, props.showArrows]);
+
+  // Autoplay configuration
+  const [autoplayInterval, setAutoplayInterval] = useState(3000); // Adjust the autoplay interval as needed
+
+  useEffect(() => {
+    // Start autoplay when the component mounts
+    const autoplayTimer = setInterval(() => {
+      // Calculate the next slide index, wrapping around to the first slide if needed
+      const nextSlide = (goToSlide + 1) % cards.length;
+      setGoToSlide(nextSlide);
+    }, autoplayInterval);
+
+    // Clear the autoplay timer when the component unmounts
+    return () => {
+      clearInterval(autoplayTimer);
+    };
+  }, [cards, goToSlide, autoplayInterval]);
+
   return (
     <div
       style={{ width: props.width, height: props.height, margin: props.margin }}
