421957d924ab97af96edf02e81b781d0f514a842
Muhammad Babar <babar@MacBook-Pro-3.local>
2023-11-08 16:30:29 +0500

    Contact Us Form Inputs Color Issues Solved

--- src/components/customInput/CustomInput.jsx
+++ src/components/customInput/CustomInput.jsx
@@ -3,6 +3,15 @@ import PersonIcon from "@mui/icons-material/Person";
 import EmailIcon from "@mui/icons-material/Email";
 
 function CustomInput({ label, type, placeholder }) {
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
   return (
     <>
       <label
@@ -14,16 +23,26 @@ function CustomInput({ label, type, placeholder }) {
       <div class="relative mb-6">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
           {type === "email" ? (
-            <EmailIcon className=" dark:text-gray-400" />
+            <EmailIcon
+              className={`dark:text-gray-400 ${
+                isFocused ? "text-[#b91813]" : ""
+              }`}
+            />
           ) : (
-            <PersonIcon className=" dark:text-gray-400" />
+            <PersonIcon
+              className={`dark:text-gray-400 ${
+                isFocused ? "text-[#b91813]" : ""
+              }`}
+            />
           )}
         </div>
         <input
           type={type}
           id="input-group-1"
-          class="bg-[#d9d9d91a] border-b-[1px] border-b-white dark:text-gray-400 text-gray-900 text-sm rounded-[5px] focus:border-b-[#b91813] focus:ring-0 focus:outline-none block w-full h-[50px] pl-12 p-2.5"
+          class="bg-[#d9d9d91a] border-b-[1px] border-b-white text-white text-sm rounded-[5px] focus:border-b-[#b91813] focus:ring-0 focus:outline-none block w-full h-[50px] pl-12 p-2.5"
           placeholder={placeholder}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           required
         />
       </div>
--- src/components/customTextArea/CustomTextArea.jsx
+++ src/components/customTextArea/CustomTextArea.jsx
@@ -12,7 +12,7 @@ function CustomTextArea({label, placeholder}) {
       <div class="relative mb-6">
         <textarea
           id="input-group-1"
-          class="bg-[#d9d9d91a] border-b-[1px] border-b-white dark:text-gray-400 text-gray-900 text-sm rounded-[5px] focus:border-b-[#b91813] focus:ring-0 focus:outline-none block w-full h-[150px] p-2.5"
+          class="bg-[#d9d9d91a] border-b-[1px] border-b-white text-white text-sm rounded-[5px] focus:border-b-[#b91813] focus:ring-0 focus:outline-none block w-full h-[150px] p-2.5"
           placeholder={placeholder}
           required
         />
