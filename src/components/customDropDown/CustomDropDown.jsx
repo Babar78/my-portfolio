e1e2cc624af938be8a6dd2b25791e42908a6da63
Muhammad Babar <babar@MacBook-Pro-3.local>
2023-12-09 04:13:10 +0500

    Images Grid Created for Graphics Design Projects

---
+++ src/components/customDropDown/CustomDropDown.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+function CustomDropDown() {
+  const [category, setCategory] = React.useState("All Images");
+
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  return (
+    <FormControl>
+      <Select
+        className="border-white bg-[#47556950] text-white w-[220px] pr-2"
+        id="selectgraphicProjectCategory"
+        value={category}
+        onChange={handleChange}
+        IconComponent={() => <ArrowDropDownIcon style={{ color: "white" }} />}
+        style={{ border: "1px solid white", color: "white", height: "50px" }}
+      >
+        <MenuItem value="All Images">All Images</MenuItem>
+        <MenuItem value="Logo Designs">Logo Designs</MenuItem>
+        <MenuItem value="Social Media Posts">Social Media Posts</MenuItem>
+        <MenuItem value="Merchandise<">Merchandise</MenuItem>
+        <MenuItem value="Brandwall">Brandwall</MenuItem>
+        <MenuItem value="Letterhead">Letterhead</MenuItem>
+        <MenuItem value="Event Ticket">Event Ticket</MenuItem>
+        <MenuItem value="Appreciation Certificate">
+          Appreciation Certificate
+        </MenuItem>
+        <MenuItem value="Broucher">Broucher</MenuItem>
+        <MenuItem value="Broucher">Broucher</MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
+export default CustomDropDown;
---
+++ src/components/mansoryPicturesGrid/MansoryPicturesGrid.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Masonry from "@mui/lab/Masonry";
+
+const Item = styled(Paper)(({ theme, imgSrc }) => ({
+  position: "relative",
+  background: `url(${imgSrc})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  overflow: "hidden",
+  "&::before": {
+    content: '""',
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: "rgba(0, 0, 0, 0.3)",
+    transition: "background 0.3s",
+  },
+  "&:hover::before": {
+    background: "rgba(0, 0, 0, 0.5)", // Adjust the opacity as needed
+  },
+}));
+
+function MansoryPicturesGrid({ imagesData }) {
+  // Function to generate random heights with a minimum value
+  const generateRandomHeight = (min) => {
+    return Math.max(Math.floor(Math.random() * 120) + min, min);
+  };
+
+  const generateRandomHeights = (count, minHeight) => {
+    return Array.from({ length: count }, () => generateRandomHeight(minHeight));
+  };
+
+  const heights = generateRandomHeights(imagesData.length, 150);
+
+//   Responsive breakpoints
+const [breakpointCols, setBreakpointCols] = React.useState(4);
+
+React.useEffect(() => {
+    const handleResize = () => {
+        if (window.innerWidth >= 1280) {
+        setBreakpointCols(4);
+        } else if (window.innerWidth >= 960) {
+        setBreakpointCols(3);
+        } else if (window.innerWidth >= 600) {
+        setBreakpointCols(2);
+        } else {
+        setBreakpointCols(1);
+        }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+}, []);
+
+  return (
+    <Box>
+      <Masonry columns={breakpointCols} spacing={2}>
+        {imagesData.map((imageItem, index) => (
+          <Item
+            key={index}
+            sx={{ height: heights[index], padding: "0 !important" }}
+            imgSrc={imageItem.src}
+            className="relative flex items-end pictureItem cursor-pointer"
+          >
+            <div className="textDetailDiv w-full flex items-center text-white bg-gradient-to-t from-[rgba(0,0,0,0.8)] to-[#0000001a]">
+              <div className="text-start">
+                <p className="text-[14px] m-0 p-0">{imageItem.companyName}</p>
+                <span className="text-[10px] m-0 p-0 text-slate-400">
+                  {imageItem.position}
+                </span>
+              </div>
+            </div>
+          </Item>
+        ))}
+      </Masonry>
+    </Box>
+  );
+}
+
+export default MansoryPicturesGrid;
