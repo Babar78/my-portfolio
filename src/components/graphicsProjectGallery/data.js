f3db6c615e6a2babe84d15c833d23aaab7c4bfd9
Muhammad Babar <babar@MacBook-Pro-3.local>
2023-12-09 16:14:00 +0500

    Graphic Project Gallery Data imported in separated data.js file

---
+++ src/components/graphicsProjectGallery/data.js
@@ -0,0 +1,102 @@
+import nlcLogo from "../../assets/images/personalProjects/ff.svg";
+import bdsLogo from "../../assets/images/personalProjects/bds.svg";
+import nfcLogo from "../../assets/images/personalProjects/bilty.svg";
+
+import img1 from "../../assets/images/services/graphicDesign.png";
+import img2 from "../../assets/images/services/ui.png";
+import img3 from "../../assets/images/services/webDev.png";
+import img4 from "../../assets/images/services/webDev.png";
+import img5 from "../../assets/images/services/ui.png";
+import img6 from "../../assets/images/services/graphicDesign.png";
+import img7 from "../../assets/images/services/graphicDesign.png";
+import img8 from "../../assets/images/services/graphicDesign.png";
+import img9 from "../../assets/images/services/graphicDesign.png";
+import img10 from "../../assets/images/services/graphicDesign.png";
+
+export const logos = {
+  nlc: nlcLogo,
+  bds: bdsLogo,
+  nfc: nfcLogo,
+};
+
+export const imagesData = [
+  {
+    src: img1,
+    companyName: "Company A",
+    position: "Designer",
+    category: "Brandwall",
+    companyLogoImg: logos.nfc,
+  },
+
+  {
+    src: img2,
+    companyName: "Company B",
+    position: "Designer",
+    category: "Merchandise",
+    companyLogoImg: logos.bds,
+  },
+
+  {
+    src: img3,
+    companyName: "Company C",
+    position: "Designer",
+    category: "Merchandise",
+    companyLogoImg: logos.bds,
+  },
+
+  {
+    src: img4,
+    companyName: "Company D",
+    position: "Designer",
+    category: "Social Media Post",
+    companyLogoImg: logos.bds,
+  },
+
+  {
+    src: img5,
+    companyName: "Company E",
+    position: "Designer",
+    category: "Social Media Post",
+    companyLogoImg: logos.bds,
+  },
+
+  {
+    src: img6,
+    companyName: "Company F",
+    position: "Designer",
+    category: "Logo Design",
+    companyLogoImg: logos.nlc,
+  },
+
+  {
+    src: img7,
+    companyName: "Company G",
+    position: "Designer",
+    category: "Logo Design",
+    companyLogoImg: logos.nlc,
+  },
+
+  {
+    src: img8,
+    companyName: "Company H",
+    position: "Designer",
+    category: "Logo Design",
+    companyLogoImg: logos.nlc,
+  },
+
+  {
+    src: img9,
+    companyName: "Company I",
+    position: "Designer",
+    category: "Logo Design",
+    companyLogoImg: logos.nlc,
+  },
+
+  {
+    src: img10,
+    companyName: "Company J",
+    position: "Designer",
+    category: "Logo Design",
+    companyLogoImg: logos.nlc,
+  },
+];
