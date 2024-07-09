1a2c41cd065d89b43b8b40f865abe89f133e635e
Babar78 <muhammadbabarashraf8@gmail.com>
2024-07-09 18:36:51 +0500

    refactor: Update CV file name for download

--- src/components/customButton/CustomButton.jsx
+++ src/components/customButton/CustomButton.jsx
@@ -6,7 +6,7 @@ function CustomButton({ label, icon, downloadPdf, pdfFile }) {
   return downloadPdf ? (
     <a
       href={pdfFile}
-      download="Resume-Muhammad-Babar.pdf"
+      download="Resume_Muhammad_Babar.pdf"
       target="_blank"
       rel="noreferrer"
       className="btn bg-[#b91813] text-white transition ease-in delay-150 duration-75 hover:text-black inline-flex items-center p-4 text-base"
